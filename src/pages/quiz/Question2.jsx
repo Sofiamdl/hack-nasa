b3eca302fcaead35a01a14253ce09f4938eb6c14
apfdamascena <apfd@cin.ufpe.br>
2023-10-08 17:44:53 -0300

    feature(start-animating): code

--- src/pages/quiz/Question2.jsx
+++ src/pages/quiz/Question2.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Question1.css';
 import Cursor from "../../components/CustomCursor/index"
 import Tarot from "../../newassets/tarotj.png"
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 import LeftCard from "../../newassets/left-card.png"
 import RightCard from "../../newassets/right-card.png"
 
+import gsap from 'gsap';
+
 function Question2() {
   const options = ['Chill', 'Adventurous'];
   const [selectedOption, setSelectedOption] = useState('');
@@ -14,6 +16,59 @@ function Question2() {
   const handleOptionSelected = (option) => {
     setSelectedOption(option);
   };
+
+  useEffect(() => {
+    addCardEventListener()
+  }, []);
+
+  const addCardEventListener = () => {
+    const left = document.getElementById('left-card-image');
+    const right = document.getElementById('right-card-image');
+
+    left.addEventListener("mouseenter", () => {
+      gsap.to("#left-card", {
+        x: "-30px",
+        y: "-70px",
+        scrubs: true,
+        rotate:  15,
+        duration: 1,
+      })
+    })
+
+    left.addEventListener("mouseleave", () => {
+      gsap.to("#left-card", {
+        x: "0px",
+        y: "0px",
+        scrubs: true,
+        rotate: -15 ,
+        duration: 1
+      })
+    })
+
+    right.addEventListener("mouseenter", () => {
+      gsap.to("#right-card", {
+        x: "30px",
+        y: "-70px",
+        scrubs: true,
+        rotate: -15,
+        duration: 1
+      })
+    })
+
+    right.addEventListener("mouseleave", () => {
+      gsap.to("#right-card", {
+        x: "0px", 
+        y: "0px",
+        scrubs: true,
+        rotate: 15,
+        duration: 1 
+      })
+    })
+
+    
+  }
+
+  
 
   return (
     <div className="container2">
@@ -26,12 +81,13 @@ function Question2() {
       </div>
 
       <div className="options2">
-        {options.map((option) => (
+        {options.map((option, index) => (
           <Link
             key={option}
             className={`option ${selectedOption === option ? 'selected' : ''}`}
             onClick={() => handleOptionSelected(option)}
             to={"/q3"}
+            id={index === 0 ? "left-card-image" : "right-card-image"}
           >
             {option}
           </Link>
